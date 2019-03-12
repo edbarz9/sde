8d050ede2f22b67121cc3f372ddba73de6754f96
edbarz9 <charles@tinson.cc>
2019-03-12 16:04:21 +0100

    soup created with cocovoit as exemple query

--- yt/search.py
+++ yt/search.py
@@ -1,9 +1,18 @@
 from urllib.request import urlopen as uOpn
 from bs4 import BeautifulSoup as bsoup
 
+query = "cocovoit"
+
+ytSearchBase = "https://www.youtube.com/results?search_query="
+
+url = ytSearchBase + query
+
 def soupMaker(url):
     uClient = uOpn(url)
     html_page = uClient.read()
     uClient.close()
     soup = bsoup(html_page,"html.parser")
     return soup
+
+searchResultSoup = soupMaker(url)
+print(searchResultSoup)
