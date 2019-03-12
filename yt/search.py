b746556c66564c3b8fe854bd276791d6670b0021
edbarz9 <charles@tinson.cc>
2019-03-12 16:01:43 +0100

    soupMaker func created

--- yt/search.py
+++ yt/search.py
@@ -1,2 +1,9 @@
 from urllib.request import urlopen as uOpn
 from bs4 import BeautifulSoup as bsoup
+
+def soupMaker(url):
+    uClient = uOpn(url)
+    html_page = uClient.read()
+    uClient.close()
+    soup = bsoup(html_page,"html.parser")
+    return soup
