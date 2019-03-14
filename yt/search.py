39f3ee892499cb98dd341374c752d28bb6037030
edbarz9 <charles@tinson.cc>
2019-03-14 12:36:07 +0100

    search query is now defined by argv

--- yt/search.py
+++ yt/search.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 import os
+import sys
 from urllib.request import urlopen as uOpn
 from urllib.request import urlretrieve as uRtv
 from bs4 import BeautifulSoup as bsoup
 
-query = "cocovoit"
+def parseQuery():
+    query = ""
+    for i in range(len(sys.argv) - 2):
+        query = query + sys.argv[i+1] + "+"
+
+    query = query + sys.argv[i+2]
+    return query
+
+query = parseQuery()
 
 ytSearchBase = "https://www.youtube.com/results?search_query="
 
@@ -27,9 +36,9 @@ searchResultList = searchResults.find_all('div', {'class':'yt-lockup yt-lockup-t
 
 for i in searchResultList:
     link = i.find('a').get('href').replace('/watch?v=','')
-    print(link)
+    #print(link)
     thumb = i.find('img').get('data-thumb')
-    print(thumb)
+    #print(thumb)
     try:
         uRtv(str(thumb), thumbPath + link + ".jpg")
     except:
