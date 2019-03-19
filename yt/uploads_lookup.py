7e63e5528fc50fe4de5186b5117082f6b5422687
edbarz9 <charles@tinson.cc>
2019-03-19 09:01:01 +0100

    months dictionary created

--- yt/uploads_lookup.py
+++ yt/uploads_lookup.py
@@ -17,6 +17,8 @@ sublist = json.loads(subListJson)
 for i in sublist:
     print(i + " " + sublist[i])
 
+months = {"Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06","Jul":"07","Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"}
+
 def soupMaker(url):
     uClient = uOpn(url)
     html_page = uClient.read()
