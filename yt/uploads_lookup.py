5edb2827aa3b64254db64cfd6517f3b76ffaa099
edbarz9 <charles@tinson.cc>
2019-03-27 19:49:28 +0100

    updating json last update

--- yt/uploads_lookup.py
+++ yt/uploads_lookup.py
@@ -11,14 +11,19 @@ parser = ConfigParser()
 parser.read(confpath)
 
 subListPath = parser.get('youtube', 'subscriptions')
-subListJson = open(os.path.expanduser(subListPath)).read()
+jsonPath = os.path.expanduser(subListPath)
+subListJson = open(jsonPath).read()
 data = json.loads(subListJson)
 sublist = data['subs']
 lastCheck = data['last']
+newLast = lastCheck
 ytwatch = "https://www.youtube.com/watch?v="
 
 def dateIndxMkr(date):
+    global newLast
     dateIndex = 999999 - int(date[2:4]+date[5:7]+date[8:10])
+    if dateIndex < lastCheck:
+        newLast = dateIndex
     return dateIndex
 
 def soupMaker(url):
@@ -38,24 +43,36 @@ def parseVids(channel):
     tlist = soup.find_all('div', \
         {'class':'yt-lockup-dismissable'})
     for l in tlist:
-        link = l.find('a').get('href').replace('/watch?v=','')
-        thumb = l.find('img').get('src')
-        titletag = l.find('h3',{'class':'yt-lockup-title'})
-        title = titletag.find('a').text.strip()
-        duration = l.find('span', \
-            {'class':'video-time'}).text.strip()
-        vidsoup = soupMaker(ytwatch + link)
-        datestr = vidsoup.find('meta', \
-            {'itemprop':'datePublished'}).get("content")
-        date = dateIndxMkr(datestr)
-        vidName = str(date) + " " + link + " " + \
-                  title + " " + duration
-        print(vidName)
-        if date >= lastCheck:
-            break
+        try :
+            link = l.find('a').get('href').replace('/watch?v=','')
+            thumb = l.find('img').get('src')
+            titletag = l.find('h3',{'class':'yt-lockup-title'})
+            title = titletag.find('a').text.strip()
+            duration = l.find('span', \
+                {'class':'video-time'}).text.strip()
+            vidsoup = soupMaker(ytwatch + link)
+            datestr = vidsoup.find('meta', \
+                {'itemprop':'datePublished'}).get("content")
+            date = dateIndxMkr(datestr)
+            vidName = str(date) + " " + link + " " + \
+                      title + " " + duration
+            if date >= lastCheck:
+                break
+            print(vidName)
+        except:
+            pass
 
 def parseSub(channels):
     for c in channels:
         parseVids(channels[c])
 
+def updateJson():
+    global data
+    data['last'] = newLast
+    with open(jsonPath, 'w') as newJSON:
+        json.dump(data, newJSON, sort_keys=True, indent=2)
+
 parseSub(sublist)
+updateJson()
+print(lastCheck)
+print(newLast)
