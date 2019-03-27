3b22084cec8a732ff3f3a078336684b268c4ffc2
edbarz9 <charles@tinson.cc>
2019-03-27 19:04:44 +0100

    small clean

--- yt/uploads_lookup.py
+++ yt/uploads_lookup.py
@@ -15,10 +15,6 @@ subListJson = open(os.path.expanduser(subListPath)).read()
 data = json.loads(subListJson)
 sublist = data['subs']
 lastCheck = data['last']
-#for i in sublist:
-#    print(i + " " + sublist[i])
-
-months = {"Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06","Jul":"07","Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"}
 
 def dateIndxMkr(date):
     dateIndex = 999999 - int(date[2:4]+date[5:7]+date[8:10])
